59ddd334bee7619daf6b029c9589dc48c6d3bd11
GuiiZ1n <guigui_araujo@hotmail.com>
2025-04-26 23:51:42 +0100

    inciando o projeto da calculadora

---
+++ scripts/controler/CalcController.js
@@ -0,0 +1,61 @@
+class CalcController {
+
+    constructor(){
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this.currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+
+        setInterval(()=>{
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+        }, 1000);
+     
+
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate (){
+        return this._dateEl.innerHTML;
+    }
+
+    
+    set displayDate (value){
+        return this._dateEl.innerHTML = value;
+    }
+
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;   
+    }
+
+    get currentDate(){
+        return new Date();
+    }
+
+    set dataAtual(value){
+        this._dataAtual = value;
+    }
+
+}
