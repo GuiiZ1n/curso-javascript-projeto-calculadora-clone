03ee0c2324c7d76d60046a578ef7a7f5117d0669
GuiiZ1n <guigui_araujo@hotmail.com>
2025-04-27 00:57:27 +0100

    Projeto calculdora
    Criando métodos.

--- scripts/controler/CalcController.js
+++ scripts/controler/CalcController.js
@@ -7,21 +7,63 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this.currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
 
+        this.setDisplayDateTime();
 
-        setInterval(()=>{
+            setInterval(()=>{
 
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-            
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+                this.setDisplayDateTime();
 
         }, 1000);
-     
+    }
+
+        addEventListenerAll(element, events, fn){
+
+            events.split(' ').forEach(event => {
+
+                element.addEventListener(event, fn, false);
+            });
 
     }
+
+        initButtonsEvents(){
+
+            let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+            buttons.forEach((btn, index)=>{
+
+                this.addEventListenerAll(btn, 'click drag mouseover', e=> {
+
+                    console.log(btn.className.baseVal.replace("btn-", ""));
+
+                });
+
+                this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                    btn.style.cursor = "pointer";
+
+                });
+
+            });
+
+        }
+
+                
+        setDisplayDateTime(){
+
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day: "2-digit",
+                    month: "long",
+                    year: "numeric" 
+            });      
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }
+     
+
+   
 
     get displayTime(){
         return this._timeEl.innerHTML;
